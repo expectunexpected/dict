b7851832c24881d7b3100bdb90d32e059b029f68
expectunexpected <adam.n.wski@gmail.com>
2016-01-26 18:52:18 +0100

    done
    done

--- getword.c
+++ getword.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "global.h"
 
 int getword()
 {
-int rand2,x=0,lines,i,j;
-char word[60],answ[60],c,**usedw;
+int rand2,lines=0,i,j;
+char word[60],answ[60],gword[60],c,**usedw,**usedw2;
 int p=0,pointsm=0,usedwb,z;
+srand(time(NULL));
+
 printf("%s\n",cat);
 FILE *fp;
 fp=fopen(cat, "r");
@@ -15,37 +18,53 @@ if (fp == NULL) {
     printf("Brak pliku slownika lub zawartosci.\n");
     return 0;
 }
-rewind(fp);
-while((c=getc(fp)) !=EOF)
+do
+{
+    c = fgetc(fp);
+    if(c == '\n')
     {
-        if(c=='\n')
-        lines++;
+    lines++;
     }
+} while (c != EOF);
+if(c != '\n' && lines != 0)
+{
+    lines++;
+}
 rewind(fp);
+printf("%d\n", lines);
 printf("Ile chcesz slowek?\n");
 scanf("%d",&z);
 
-usedw=(char **) malloc(z*sizeof(char *));
-
+usedw=(char **) calloc(z, sizeof(char *));
+usedw2=(char **) calloc(z, sizeof(char *));
 for(i=0;i<z;i++)
 	{
-    usedw[i]=(char *) malloc(60*sizeof(char));
-	}
+    usedw[i]=(char *) calloc(60, sizeof(char));
+    }
+for(i=0;i<z;i++)
+    {
+        usedw2[i]=(char *) calloc(60, sizeof(char));
+    }
 for(i=0;i<z;i++)
 {
     usedwb=1;
-    do
+    rand2=rand()%lines+1;
+    if(rand2%2==0)
+    {
+        if(a==1)
+        {
+            fgets(word, 60, fp);
+        }
+        else
+        {
+          rand2--;
+        }
+    }
+    for(j=0;j<rand2;j++)
     {
         fgets(word, 60, fp);
-        srand(time(NULL));
-        rand2=rand()%+1;
-        x++;
     }
-    while(rand2 != 1 & x<lines);
-    if(x%2==0 & a==1)
-    {
-        fgets(word, 60, fp);
-    }
+    strtok(word, "\n");
     for(j=0;j<p;j++)
     {
         if(usedw[j] == word)
@@ -56,33 +75,48 @@ for(i=0;i<z;i++)
     }
     if(usedwb=1)
     {
-        printf("%s -- ", word);
+        printf("\n%s - ", word);
         scanf("%s",answ);
+        strcpy(gword, word);
         fgets(word, 60, fp);
-        printf("\n%s\n",word);
-        if(word==answ)
+        strtok(word, "\n");
+        if(strcmp( word, answ) == 0)
         {
             printf("Dobra odpowiedz!\n");
             p++;
-            usedw[p]=answ;
+            strcpy(usedw[p], answ);
         }
         else
         {
         printf("Sproboj ponownie :C\n");
         pointsm++;
+        strcpy(usedw2[pointsm],word);
+        printf("\n%s-",gword);
+        printf("%s\n", word);
         }
+
     }
     rewind(fp);
 }
+for ( i = 0; i < z; i++ )
+{
+    free(usedw[i]);
+}
+free(usedw);
 printf("Slowa, ktorych nie umiales:\n");
 FILE *wrong;
 wrong = fopen("wrong.txt","w");
 for(i=0;i<pointsm;i++)
 {
-    fprintf(wrong,"%s\n",usedw[i]);
-    printf("%s\n",usedw[i]);
+    fprintf(wrong,"%s\n",usedw2[i]);
+    printf("%s\n",usedw2[i]);
 }
 fclose(wrong);
+for ( i = 0; i < z; i++ )
+{
+    free(usedw2[i]);
+}
+free(usedw2);
 system("clear");
-printf("Zle odpowiedzi - %d\nDobre odpowiedzi - %d",pointsm,p);
+printf("Zle odpowiedzi - %d\nDobre odpowiedzi - %d\n",pointsm,p);
 }
